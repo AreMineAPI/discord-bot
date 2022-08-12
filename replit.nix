d3472042055601c0c76f147604f2a4746c5bf457
clqu <76551996+clqu@users.noreply.github.com>
2022-08-12 20:42:52 +0300

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,13 @@
+{ pkgs }: {
+	deps = [
+		pkgs.sudo
+		pkgs.nodejs-16_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.yarn
+        pkgs.replitPackages.jest
+		pkgs.libuuid
+	];
+	env = { 
+		LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid]; 
+	};
+}
